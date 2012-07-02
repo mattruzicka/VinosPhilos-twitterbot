4500718dba897bd462e8f1bbf02386272335acbc
Matt Ruzicka <mjrguitar@gmail.com>
2012-07-02 17:55:42 -0400

    fixed?

--- initializer.rb
+++ initializer.rb
@@ -8,20 +8,11 @@ require 'gabbler'
 ENVIRONMENT = "development"
 APP_CONFIG = YAML.load(File.read("config/config.yml"))[ENVIRONMENT]
 
-if ENVIRONTMENT == "developmeent"
-  Twitter.configure do |config|
-    config.consumer_key = APP_CONFIG[:twitter][:consumer_key]
-    config.consumer_secret = APP_CONFIG[:twitter][:consumer_secret]
-    config.oauth_token = APP_CONFIG[:twitter][:oauth_token]
-    config.oauth_token_secret = APP_CONFIG[:twitter][:oauth_token_secret]
-  end
-else 
-  Twitter.configure do |config|
-    config.consumer_key = CONSUMER_KEY
-    config.consumer_secret = CONSUMER_SECRET
-    config.oauth_token = OAUTH_TOKEN
-    config.oauth_token_secret = OAUTH_TOKEN_SECRET
-  end
+Twitter.configure do |config|
+  config.consumer_key = APP_CONFIG[:twitter][:consumer_key]
+  config.consumer_secret = APP_CONFIG[:twitter][:consumer_secret]
+  config.oauth_token = APP_CONFIG[:twitter][:oauth_token]
+  config.oauth_token_secret = APP_CONFIG[:twitter][:oauth_token_secret]
 end
 
 # load dictionary
